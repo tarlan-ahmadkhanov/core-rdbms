02420f294d8cc89c844cfd49bb2817ef1b9bc513
= <tarlan.ahmadkhanov@pashapay.az>
2025-02-26 09:59:08 +0400

    Initial commit, project just created

---
+++ src/main/java/az/tarlan/corerdbms/CoreRdbmsApplication.java
@@ -0,0 +1,13 @@
+package az.tarlan.corerdbms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreRdbmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreRdbmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/tarlan/corerdbms/CoreRdbmsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.tarlan.corerdbms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreRdbmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
